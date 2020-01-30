09667ce0cb688cb2176059a43475a73644325cd2
Jose Montero <josemariodev@gmail.com>
2020-01-30 14:19:11 -0600

    BST day one in progress

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
 from dll_stack import Stack
+from dll_queue import Queue
 
 
 class BinarySearchTree:
@@ -12,21 +12,48 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value and self.left is None:
+            self.left = BinarySearchTree(value)
+        elif value >= self.value and self.right is None:
+            self.right = BinarySearchTree(value)
+        elif value < self.value and self.left is not None:
+            self.left.insert(value)
+        elif value >= self.value and self.right is not None:
+            self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        elif target < self.value and self.left is None:
+            return False
+        elif target >= self.value and self.right is None:
+            return False
+        elif target < self.value and self.left is not None:
+            self = self.left
+            return self.contains(target)
+        elif target >= self.value and self.right is not None:
+            self = self.right
+            return self.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        while self.right is not None:
+            self = self.right
+
+        return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+   
 
     # DAY 2 Project -----------------------
 
